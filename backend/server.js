a6050078dabc4903239c1783ec4b6c53588a2339
Veevek Dave <veevekd401@gmail.com>
2023-11-13 02:12:40 -0500

    Schema

--- backend/server.js
+++ backend/server.js
@@ -12,68 +12,28 @@ app.use(express.json());
 
 //MongoDB connection
 const mongoose = require('mongoose');
-//Might need to change this user from jyoungbar02 to some sort of admin account to hand over
-// mongoose.connect("mongodb+srv://jyoungbar02:VvUQMIUhjrqViALD@solar-incentives.08p60z2.mongodb.net/?retryWrites=true&w=majority");
-const connection = mongoose.createConnection("mongodb+srv://jyoungbar02:VvUQMIUhjrqViALD@solar-incentives.08p60z2.mongodb.net/?retryWrites=true&w=majority");
-const schema = new mongoose.Schema({index: Number/*,*/ /*put incentive data here*/});
-const Adder = mongoose.model('Adders', schema);
-const EC = mongoose.model('Energy-Communities', schema);
-const TC = mongoose.model('Tribal-Communities', schema);
-const timeSchema = new mongoose.Schema({/*_id: {type: ObjectID, required: true}, index: {type: Int32, required: true},*/ milliseconds: {type: Number/*INT32*/, required: true}, year: {type: Number/*INT32*/, required: true}, month: {type: Number/*INT32*/, required: true}, day: {type: Number/*INT32*/, required: true}});
-const LastUpdated = /*mongoose*/connection.model('Last-Updated', timeSchema);
+const fs = require('fs');
+const ZipModel = require('./Models/zipModel');
+const mongoUri = 'mongodb+srv://jyoungbar02:VvUQMIUhjrqViALD@solar-incentives.08p60z2.mongodb.net/?retryWrites=true&w=majority'; 
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+// const schema = new mongoose.Schema({index: Number/*,*/ /*put incentive data here*/});
+// const Adder = mongoose.model('Adders', schema);
+// const EC = mongoose.model('Energy-Communities', schema);
+// const TC = mongoose.model('Tribal-Communities', schema);
+// const timeSchema = new mongoose.Schema({/*_id: {type: ObjectID, required: true}, index: {type: Int32, required: true},*/ milliseconds: {type: Number/*INT32*/, required: true}, year: {type: Number/*INT32*/, required: true}, month: {type: Number/*INT32*/, required: true}, day: {type: Number/*INT32*/, required: true}});
+// const LastUpdated = /*mongoose*/connection.model('Last-Updated', timeSchema);
 
-const fs = require("fs");
-const { parse } = require("csv-parse");
-app.use(bodyParser.json());
-const path = require('path');
-fs.createReadStream("./data/mydata.csv")
-  .pipe(parse({ delimiter: ",", from_line: 2 }))
-  .on("data", async function (row) {
-//    Create an instance of the DataPath model and save the data to the database
-    const data = new PowerPlant({
-      country: row[0],
-      country_long: row[1],
-      name: row[2],
-      gppd_idnr: row[3],
-      capacity_mw: row[4],
-      latitude: row[5],
-      longitude: row[6],
-      primary_fuel: row[7],
-      other_fuel1: row[8],
-      other_fuel2: row[9],
-      other_fuel3: row[10],
-      commissioning_year: row[11],
-      owner: row[12],
-      source: row[13],
-      url: row[14],
-      geolocation_source: row[15],
-      wepp_id: row[16],
-      year_of_capacity_data: row[17],
-      generation_gwh_2013: row[18],
-      generation_gwh_2014: row[19],
-      generation_gwh_2015: row[20],
-      generation_gwh_2016: row[21],
-      generation_gwh_2017: row[22],
-      generation_gwh_2018: row[23],
-      generation_gwh_2019: row[24],
-      generation_data_source: row[25],
-      estimated_generation_gwh_2013: row[26],
-      estimated_generation_gwh_2014: row[27],
-      estimated_generation_gwh_2015: row[28],
-      estimated_generation_gwh_2016: row[29],
-      estimated_generation_gwh_2017: row[30],
-      estimated_generation_note_2013: row[31],
-      estimated_generation_note_2014: row[32],
-      estimated_generation_note_2015: row[33],
-      estimated_generation_note_2016: row[34],
-      estimated_generation_note_2017: row[35],
-    });
-//     console.log(data.country_long);
-//     // await data.save(); // Save the data to the databaseß
-  })
-  .on("end", function () {
-    console.log("done");
-  });
+app.post('/import-json', async (req, res) => {
+  try {
+    const data = await fs.readFile('./Data/test.json', 'utf8');
+    const jsonData = JSON.parse(data);
+    await ZipModel.insertMany(jsonData);
+    res.send("Data imported successfully");
+  } catch (error) {
+    console.error("Error during file reading or data import", error);
+    res.status(500).send("An error occurred during the import");
+  }
+});
 
 //serve front end
 app.get('*', (req, res) => {
@@ -86,14 +46,14 @@ app.get('*', (req, res) => {
 
 //refresh database
 async function refreshDB() {
-    const today = new Date();
-    const data = await /*new LastUpdated({milliseconds: today.getTime(), year: today.getFullYear(), month: today.getMonth(), day: today.getDate()});*/ LastUpdated.find();
-    console.log(data);
-    // await data.save();
-    if((await LastUpdated.find().where('milliseconds').lt(today.getTime()-2628000000).exec()) != []) {
-        console.log("works");
-        console.log(await LastUpdated.find().where('milliseconds').lt(today.getTime()-2628000000).exec());
-    }
+    // const today = new Date();
+    // const data = await /*new LastUpdated({milliseconds: today.getTime(), year: today.getFullYear(), month: today.getMonth(), day: today.getDate()});*/ LastUpdated.find();
+    // console.log(data);
+    // // await data.save();
+    // if((await LastUpdated.find().where('milliseconds').lt(today.getTime()-2628000000).exec()) != []) {
+    //     console.log("works");
+    //     console.log(await LastUpdated.find().where('milliseconds').lt(today.getTime()-2628000000).exec());
+    // }
 }
 //test
 //all incentives endpoint
